e9c81c831594d4459ca70a0e938c10f0e1b32c6e
S2O3 <18250370540@163.com>
2024-09-16 09:40:18 +0800

    refractor

--- main.go
+++ main.go
@@ -10,9 +10,6 @@ import (
 )
 
 func main() {
-	// fmt.Println(os.Args)
-	// fmt.Println("len(os.Args):", len(os.Args))
-	// fmt.Println(os.Args[1:])
 	DoCommand(os.Args[1:])
 }
 
@@ -23,27 +20,21 @@ func DoCommand(args []string) {
 	}
 	from := args[0]
 	to := args[1]
-
+	var newName string
 	if to[0] == '.' {
-		DoRenameExt(from, to)
-		return
+		newName = DoRenameExt(from, to)
 	} else if to[0] == ':' {
-		DoTransform(from, to)
-		return
+		newName = DoTransform(from, to, args)
+	} else {
+		newName = DoRename(from, to)
 	}
-
-	//do: rename /tmp/a.mp3 b
-	//will rename /tmp/a.mp3 to /tmp/b.mp3
-	dir := path.Dir(from)
-	basename := path.Base(from)
-	ext := path.Ext(basename)
-	newname := dir + "/" + to + ext
-	if err := os.Rename(from, newname); err != nil {
+	if err := os.Rename(from, newName); err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	} else {
-		fmt.Printf("Rename success: %s -> %s\n", from, newname)
+		fmt.Printf("Rename success: %s -> %s\n", from, newName)
 	}
+
 }
 
 func ShowHelp() {
@@ -52,22 +43,17 @@ func ShowHelp() {
 
 // do: rename /tmp/a.mp3 .txt
 // will rename /tmp/a.mp3 to /tmp/a.txt
-func DoRenameExt(from, to string) {
+func DoRenameExt(from, to string) string {
 	dir := path.Dir(from)
 	basename := path.Base(from)
 	//if name is music.mp3 then filename is music and ext is .mp3
 	filename := strings.Split(basename, ".")[0]
 	newext := to
 	newname := dir + "/" + filename + newext
-	if err := os.Rename(from, newname); err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(2)
-	} else {
-		fmt.Printf("Rename success: %s -> %s\n", from, newname)
-	}
+	return newname
 }
 
-func DoTransform(from, to string) {
+func DoTransform(from, to string, args []string) string {
 	dir := path.Dir(from)
 	basename := path.Base(from)
 	filename := strings.Split(basename, ".")[0]
@@ -85,17 +71,24 @@ func DoTransform(from, to string) {
 		filename = strcase.ToKebab(filename)
 	case ":camel_lower":
 		filename = strcase.ToLowerCamel(filename)
-
+	case ":plus":
+		filename = filename + args[2]
 	default:
 		fmt.Println("Unknown transform:", to)
 		os.Exit(3)
 	}
 
 	newname := dir + "/" + filename + ext
-	if err := os.Rename(from, newname); err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(3)
-	} else {
-		fmt.Printf("Rename success: %s -> %s\n", from, newname)
-	}
+	return newname
+
+}
+
+func DoRename(from, to string) string {
+	//do: rename /tmp/a.mp3 b
+	//will rename /tmp/a.mp3 to /tmp/b.mp3
+	dir := path.Dir(from)
+	basename := path.Base(from)
+	ext := path.Ext(basename)
+	newname := dir + "/" + to + ext
+	return newname
 }
